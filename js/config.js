48aae06b6809615d7821a0f26ac1314a0cd82527
Akinloye Aremu <106943023+AkinloyeA@users.noreply.github.com>
2023-03-27 15:17:33 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
     region: "us-east-1", // e.g. us-east-2
   },
   api: {
-    invokeUrl: "https://main.d182ddv0ofu90c.amplifyapp.com/", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    invokeUrl: "https://0768ja80b8.execute-api.us-east-1.amazonaws.com/PROD", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
   },
 };
